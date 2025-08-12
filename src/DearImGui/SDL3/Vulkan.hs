579d836f1cb1e62223624f298849f0101b21c7c3
Axis Sivitz <aosivitz@gmail.com>
2025-08-12 13:46:14 -0400

    SDL3 Support
    sdl3 flag defaults to 'False' as the sdl3-hs library is currently not
    yet on hackage

---
+++ src/DearImGui/SDL3/Vulkan.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+{-|
+Module: DearImGui.SDL3.Vulkan
+
+Initialising the Vulkan backend for Dear ImGui using SDL3.
+-}
+
+module DearImGui.SDL3.Vulkan
+  ( sdl3InitForVulkan )
+  where
+
+-- inline-c
+import qualified Language.C.Inline as C
+
+-- inline-c-cpp
+import qualified Language.C.Inline.Cpp as Cpp
+
+-- sdl3
+import SDL.Video
+  ( SDLWindow(..) )
+
+-- transformers
+import Control.Monad.IO.Class ( MonadIO, liftIO )
+
+import Foreign (castPtr)
+
+
+C.context Cpp.cppCtx
+C.include "imgui.h"
+C.include "backends/imgui_impl_vulkan.h"
+C.include "backends/imgui_impl_sdl3.h"
+C.include "<SDL3/SDL.h>"
+C.include "<SDL3/SDL_vulkan.h>"
+Cpp.using "namespace ImGui"
+
+
+-- | Wraps @ImGui_ImplSDL3_InitForVulkan@.
+sdl3InitForVulkan :: MonadIO m => SDLWindow -> m Bool
+sdl3InitForVulkan (SDLWindow windowPtr) = liftIO do
+  let ptr = castPtr windowPtr
+  ( 0 /= ) <$> [C.exp| bool { ImGui_ImplSDL3_InitForVulkan((SDL_Window*)$(void* ptr)) } |]
